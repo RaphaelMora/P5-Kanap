a5a9a111645e356d87b2cac7a6eaa42c55204642
RaphaelMora <mora.raphael@gmail.com>
2022-11-28 16:25:59 +0100

    Delete confirmation.js

--- front/js/confirmation.js
+++
@@ -1,6 +0,0 @@
-let params = new URLSearchParams(document.location.search);
-let id = params.get("orderId");
-
-const orderId = document.getElementById("orderId");
-orderId.textContent = id;
-alert("Merci pour votre commande");
