ddfe4c8025aec06ba7af1de67608ed9567ebbf47
RaphaelMora <mora.raphael@gmail.com>
2022-11-29 12:15:00 +0100

    Add files via upload

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,7 +2,6 @@ let showId = localStorage.getItem("orderId");
 
 let orderId = document.getElementById("orderId");
 orderId.innerText = showId;
-localStorage.clear();
 
 alert("Merci pour votre commande");
 localStorage.clear();
