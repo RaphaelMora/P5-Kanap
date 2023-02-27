7032bb5e7dede9b6a58b8787624ba42dec72720d
RaphaelMora <mora.raphael@gmail.com>
2023-02-27 11:15:32 +0100

    Delete front/js directory

--- front/js/confirmation.js
+++
@@ -1,7 +0,0 @@
-let showId = localStorage.getItem("orderId");
-
-let orderId = document.getElementById("orderId");
-orderId.innerText = showId;
-
-alert("Merci pour votre commande");
-localStorage.clear();
